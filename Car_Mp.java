62ece2b29d8d0b5ba2767f5782855cbc1ea098ff
mohsin6837 <68821359+mohsin6837@users.noreply.github.com>
2022-07-08 22:43:06 +0530

    Update Car_Mp.java

--- Car_Mp.java
+++ Car_Mp.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 
 public class Car_Mp {
-    static float pow(float j,int n){
+    static float pow(float a, float b){
         float pow=1;
-        for (j=1;j<=n;j++){
-            pow*=j;
+        for (int i=1;i<=b;i++){
+            pow*=a;
         }
         return pow;
     }
     public static void main(String[] args) {
-        float pp,dp,ar;
-        int n;
-        System.out.println("Enter the purchase price,down payment,annual interest rate and no. of payments(which is 36,48 or 60)");
+        float pp,dp,ar,mp,P,i,n;
+        System.out.println("Enter the purchase price of the car");
         Scanner sc=new Scanner(System.in);
         pp=sc.nextFloat();
-        dp=sc.nextFloat();
+        System.out.println("Enter the down payment you have saved for the car");
+        dp= sc.nextFloat();
+        System.out.println(" Enter the annual rate of interest of the car ");
         ar=sc.nextFloat();
-        n= sc.nextInt();
-        float P=pp-dp,i=ar/12;
-        float mp=i*P/(1-(1/pow(1+i,n)));
-        System.out.println("Monthly payment of car is " + mp);
+        System.out.println("Enter the no of payments (usually 36,48 or 60)");
+        n= sc.nextFloat();
+        P=pp-dp;
+        i=ar/12;
+        mp=i*P/(1-(1/pow(1+i,n)));
+        System.out.printf("Monthly payment of car=Rs.%.2f",mp);
     }
 }
